296453c79d18fdcbf310ac44a261549fb178590f
Jacob Garland <352cjg@gmail.com>
2025-03-08 16:07:12 -0500

    configured cypress more, added test commands

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from "cypress";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 
   component: {
+    port: 5173,
     devServer: {
       framework: "react",
       bundler: "vite",
+      viteConfig,
     },
+    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 });
